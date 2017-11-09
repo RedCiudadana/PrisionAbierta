344309df3082021929fd41f83a6f3d6782c3c877
Hector Hurtarte <hectorh30@gmail.com>
2017-11-09 08:13:32 -0600

    WIP

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,52 +3,15 @@ import Ember from 'ember';
 const { computed } = Ember;
 
 export default Ember.Controller.extend({
-  currentSelector: computed(
-    'esMujer',
-    'esHombre',
-    'estaEnProceso',
-    'estaDescalificado',
-    function() {
-      if (
-        !this.get('esMujer')
-            && !this.get('esHombre')
-            && !this.get('estaEnProceso')
-            && !this.get('estaDescalificado')
-      ) {
-        return '*';
-      }
-
-      let selectors = [];
-
-      if (this.get('esMujer')) {
-        selectors.push('.mujer');
-      }
-
-      if (this.get('esHombre')) {
-        selectors.push('.hombre');
-      }
-
-      if (this.get('estaEnProceso')) {
-        selectors.push('.enProceso');
-      }
-
-      if (this.get('estaDescalificado')) {
-        selectors.push('.descalificado');
-      }
-
-      return selectors.join(', ');
-    }
-  ),
+  paises: computed('model.perfiles', function() {
+    return this.get('model.perfiles')
+      .mapBy('direccion')
+      .uniq();
+  }),
 
   actions: {
-    applyFilter() {
-      var $container = Ember.$('#portfolio');
-
-      $container.isotope({transitionDuration: '0.65s'});
-
-      $container.isotope({filter: this.get('currentSelector')});
-
-      return false;
+    selectPais() {
+      console.log('done');
     }
   }
 });
